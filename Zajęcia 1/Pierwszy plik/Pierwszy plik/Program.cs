96db14423124383ed7794f0469266270e7f2c7b4
JakubGolecJunior <kuba.golec.jr@onet.pl>
2019-10-03 12:09:49 +0200

    Pierwsze zajęcia

---
+++ Zajęcia 1/Pierwszy plik/Pierwszy plik/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pierwszy_plik
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {//string
+            
+
+            Console.WriteLine("program oblcizający pole prostokąta");
+            Console.WriteLine("Podaj zmienną A");
+
+            double a = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Podaj Zmienną B");
+
+            double b = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Pole prostokąta wynosi: " + a*b);
+
+
+
+        
+            Console.ReadKey();
+        }
+
+
+    }
+}
